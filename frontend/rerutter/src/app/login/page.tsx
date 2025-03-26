4d904ebcbc7cc4b9db07e0e3941d461869f12f04
Rerurate514 <rerurateyuto@gmail.com>
2025-03-26 21:29:36 +0900

    Loginひな形作成

---
+++ frontend/rerutter/src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import { LoginPageTemplate } from "@/components/loginTemplate"
+
+export default function LoginPage() {
+    return <LoginPageTemplate />
+}
---
+++ frontend/rerutter/src/components/loginTemplate.tsx
@@ -0,0 +1,3 @@
+export const LoginPageTemplate = () => {
+    return <h1>LOGIN</h1>
+}
